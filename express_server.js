847941f15df08c3e72683f79200615a247c63daf
IrinaGM <irina.golionko@gmail.com>
2023-04-29 20:10:09 +0000

    add route for /urls/:id. add template for it, urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -17,9 +17,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+// route to get all the URLs and their shortened forms
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+// route to get a specific URL (by it's id) and it's shortened form
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
