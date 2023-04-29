cbfac5ea75ab5aceb7d594d93b82712b7c27616e
IrinaGM <irina.golionko@gmail.com>
2023-04-29 19:55:03 +0000

    fix /urls route and implement template view for urls_index

--- express_server.js
+++ express_server.js
@@ -18,7 +18,8 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.render("urls_index", urlDatabase);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
